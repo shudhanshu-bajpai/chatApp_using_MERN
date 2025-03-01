54fb2243843970799270763a5c42a2ac705aa546
Shudhanshu Bajpai <shudhanshubajpai99@gmail.com>
2025-03-01 15:03:05 +0530

    cors origin updated

--- backend/socket/socket.js
+++ backend/socket/socket.js
@@ -33,27 +33,6 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import { Server } from "socket.io";
 import http from "http";
 import express from "express";
@@ -63,7 +42,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
 	cors: {
-		origin: ["http://localhost:5173"],
+		origin: ['https://chatapp-3i01.onrender.com'],
 		methods: ["GET", "POST"],
 	},
 });
